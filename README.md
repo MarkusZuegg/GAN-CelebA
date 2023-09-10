1c9e2d180237d19d9c99038b1c27cb49c6c1ca3f
MarkusZuegg <markuszuegg@outlook.com>
2023-09-11 02:10:38 +1000

    Introductionary code (non-functional)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GAN-CelebA
+Using the CelebA dataset to create a GAN that will generate images of faces
---
+++ Sb_run_GAN.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --time=0-00:50:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=vgpu
+#SBATCH --job-name="GAN"
+# SBATCH --mail-user=s4744924@student.uq.edu.au
+# SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --output=x%j.out
+#SBATCH --error=x%j.err
+
+module load cuda
+conda activate pytorch2
+
+srun python Run_Model.py
